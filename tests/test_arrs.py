63cd67bd4d535da31eaef028324cf63089cca46b
Sergey Malyarov <sergey@malyaroff.com>
2023-06-06 11:32:15 +0300

    Fix tests for arrs module

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,62 +1,57 @@
+import unittest
 from utils import arrs
 
-def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 2
-    assert arrs.get([], 0, "test") == "test"
+class ArrsTestCase(unittest.TestCase):
 
+    def test_get_existing_index(self):
+        array = [1, 2, 3, 4, 5]
+        index = 2
+        default_value = "Not found"
+        expected_value = 3
 
-def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+        result = arrs.get(array, index, default=default_value)
+        self.assertEqual(result, expected_value)
 
-def test_get_existing_index():
-    array = [1, 2, 3, 4, 5]
-    index = 2
-    default_value = "Not found"  # Добавлено объявление переменной default_value
-    expected_value = 3
+    def test_get_non_existing_index(self):
+        array = [1, 2, 3, 4, 5]
+        index = 5
+        default_value = "Not found"
+        expected_value = "Not found"
 
-    result = arrs.get(array, index, default=default_value)
-    assert result == expected_value
+        result = arrs.get(array, index, default=default_value)
+        self.assertEqual(result, expected_value)
 
+    def test_my_slice_default_arguments(self):
+        array = [1, 2, 3, 4, 5]
+        expected_result = [1, 2, 3, 4, 5]
 
+        result = arrs.my_slice(array)
+        self.assertEqual(result, expected_result)
 
-def test_get_non_existing_index():
-    array = [1, 2, 3, 4, 5]
-    index = 5
-    default_value = "Not found"
-    expected_value = "Not found"
+    def test_my_slice_with_start_index(self):
+        array = [1, 2, 3, 4, 5]
+        start = 2
+        expected_result = [3, 4, 5]
 
-    result = arrs.get(array, index, default=default_value)
-    assert result == expected_value
+        result = arrs.my_slice(array, start=start)
+        self.assertEqual(result, expected_result)
 
-def test_my_slice_default_arguments():
-    array = [1, 2, 3, 4, 5]
-    expected_result = [1, 2, 3, 4, 5]
+    def test_my_slice_with_end_index(self):
+        array = [1, 2, 3, 4, 5]
+        end = 3
+        expected_result = [1, 2, 3]
 
-    result = arrs.my_slice(array)
-    assert result == expected_result
+        result = arrs.my_slice(array, end=end)
+        self.assertEqual(result, expected_result)
 
-def test_my_slice_with_start_index():
-    array = [1, 2, 3, 4, 5]
-    start = 2
-    expected_result = [3, 4, 5]
+    def test_my_slice_with_negative_start_and_end(self):
+        array = [1, 2, 3, 4, 5]
+        start = -3
+        end = -1
+        expected_result = [3, 4]
 
-    result = arrs.my_slice(array, start=start)
-    assert result == expected_result
+        result = arrs.my_slice(array, start=start, end=end)
+        self.assertEqual(result, expected_result)
 
-def test_my_slice_with_end_index():
-    array = [1, 2, 3, 4, 5]
-    end = 3
-    expected_result = [1, 2, 3]
-
-    result = arrs.my_slice(array, end=end)
-    assert result == expected_result
-
-def test_my_slice_with_negative_start_and_end():
-    array = [1, 2, 3, 4, 5]
-    start = -3
-    end = -1
-    expected_result = [3, 4]
-
-    result = arrs.my_slice(array, start=start, end=end)
-    assert result == expected_result
+if __name__ == '__main__':
+    unittest.main()
